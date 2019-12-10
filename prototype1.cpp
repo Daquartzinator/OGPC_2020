975712f7a8039f886ab1f3aa5a5d7010433afc66
Daquartzinator <jasper.bushnell@gmail.com>
2019-12-10 10:56:29 -0800

    adds sfml window(basic)

--- prototype1.cpp
+++ prototype1.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
+#include<SFML/Graphics.hpp>
 using namespace std;
+using namespace sf;
 
 int main(){
 	cout << "That's how Mafia Works" << endl;
 	cout << "This is a prototype file" << endl;
+
+    RenderWindow window(VideoMode(400,200), "That's How Mafia Works");
+    window.setFramerateLimit(30);
+    while (window.isOpen()){
+        Event event;
+        while (window.pollEvent(event)){
+            if (event.type == Event::Closed){
+                window.close();
+            }
+        }
+    }
 }
