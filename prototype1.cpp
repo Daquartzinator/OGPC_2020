f0b568c69994b29427f816139db3d7b1f65fda88
Fiona :-) <47460006+snorgyenthusiast@users.noreply.github.com>
2019-12-12 11:18:54 -0800

    Added Header and add.cpp

--- prototype1.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-	cout << "That's how Mafia Works" << endl;
-}
---
+++ testingProject/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "add.h"
+using namespace std;
+
+int main(){
+    cout<<"3+4 is "<<add(3,4)<<"\n";
+    //add is declared in add.h and the function is in add.cpp
+    return 0;
+}
